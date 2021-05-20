5cbc4e477e35bd59ddab75da0e7c895e2f33b69d
letuankiet212 <48205415+letuankiet212@users.noreply.github.com>
2021-05-20 22:19:15 +0700

    up

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Nav class="background-white pt-5">
+      <NavbarTitle class="t-center t-upper t-bold color-gray">Main</NavbarTitle>
+    </Nav>
+  </div>
+
+</template>
+
+<script>
+import {Nav,NavbarTitle} from "@/components/Navbar/NavbarElements";
+
+export default {
+  name: "Navbar",
+  components:
+      {
+        Nav,NavbarTitle
+      }
+
+}
+</script>
+
---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,9 @@
+import styled from "vue-styled-components";
+
+export const Nav = styled.div`
+  min-height: 100vh;
+  width: 22.8rem;
+  box-shadow: 4px 0 5px #00000040;
+`
+export const NavbarTitle = styled.h4`
+`
